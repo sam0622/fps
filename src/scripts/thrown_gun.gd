2e31d28d67e2bf5bc9b7952695c4c22a5992c1bd
Sam <140025368+sam0622@users.noreply.github.com>
2025-04-11 11:34:14 -0400

    i have no clue

--- src/scripts/thrown_gun.gd
+++ src/scripts/thrown_gun.gd
@@ -13,23 +13,28 @@ func _ready() -> void:
 	global_position = player.throw_marker.global_position
 	rotation.z = player.current_gun.rotation.z
 	apply_central_impulse(-player.throw_marker.global_transform.basis.z * forward_velocity)
-	self.angular_velocity = Vector3(10, 0, 0)
+	#angular_velocity = Vector3(10, 10, 10)
+	sleeping = false
 
 
-func _on_body_entered(body: Node) -> void:
-	angular_velocity = Vector3.ZERO
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player or body is ThrownGun:
+		return
+	print("aaaaaaaaaaaa")
 	if active:
+		print(body.to_string())
+		if body.is_in_group("shootable"):
+			body.hit()
 		if body.is_in_group("enemy"):
 			body.hit(damage, GameManager.DamageTypes.BLUNT)
-	
 	if bounced:
 			# On second impact kill velocity
 			active = false
 			linear_velocity = Vector3.ZERO
 			angular_velocity = Vector3.ZERO
-			sleeping = true
 			await get_tree().create_timer(10.0).timeout
 			queue_free()
 	else:
 		bounced = true
 		linear_velocity *= 0.7
+		linear_velocity = -linear_velocity
